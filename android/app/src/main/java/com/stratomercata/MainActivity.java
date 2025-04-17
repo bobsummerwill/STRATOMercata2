5d0dc52ae05d16b2aa12c2814264bcb6daee6210
Bob Summerwill <bob@summerwill.net>
2025-04-17 16:04:46 -0400

    Cleaned up unreferenced files

--- android/app/src/main/java/com/stratomercata/MainActivity.java
+++ android/app/src/main/java/com/stratomercata/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private HeaderView headerView;
     private GoldPriceView goldPriceView;
     private PriceChartView priceChartView;
     private TradeFormView tradeFormView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        headerView = findViewById(R.id.header_view);
         goldPriceView = findViewById(R.id.gold_price_view);
         priceChartView = findViewById(R.id.price_chart_view);
         tradeFormView = findViewById(R.id.trade_form_view);
